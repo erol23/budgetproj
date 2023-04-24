eac8dd2f0c67c37e3f6bedd9eac3a49289ca8430
erol23 <hurkanrol15@gmail.com>
2023-04-24 17:07:29 +0200

    added vernav

--- src/components/VerNav.jsx
+++ src/components/VerNav.jsx
@@ -1,22 +1,29 @@
-import React from 'react'
+import { FaTimes } from "react-icons/fa";
 
-const VerNav = () => {
+const VerNav = ({ nav, handleClick }) => {
   return (
-    <div className='bg-black opacity-80 h-screen fixed z-[1]'>
-        <ul className='h-full z-10'>
-            <li>Hello World</li>
-            <li>Hello World</li>
-            <li>Hello World</li>
-            <li>Hello World</li>
-            <li>Hello World</li>
-            <li>Hello World</li>
-            <li>Hello World</li>
-            <li>Hello World</li>
-            <li>Hello World</li>
-            <li>Hello World</li>
+    <div className={nav ? "bg-black opacity-80 h-screen fixed z-[1] w-full transition ease-linear duration-1000" : "hidden"}>
+      <div className="h-full z-10 w-1/3 bg-white">
+        <div className="flex justify-end">
+          <button onClick={handleClick}>
+            <FaTimes />
+          </button>
+        </div>
+        <ul className="w-full">
+          <li>Hello World</li>
+          <li>Hello World</li>
+          <li>Hello World</li>
+          <li>Hello World</li>
+          <li>Hello World</li>
+          <li>Hello World</li>
+          <li>Hello World</li>
+          <li>Hello World</li>
+          <li>Hello World</li>
+          <li>Hello World</li>
         </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default VerNav
+export default VerNav;
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,32 +1,34 @@
-import React, { useState } from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from '../pages/Home'
-import Navbar from '../components/Navbar'
-import User from '../pages/User'
-import Login from '../pages/Login'
-import Register from '../pages/Register'
-import PrivateRouter from './PrivateRouter'
+import React, { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import Navbar from "../components/Navbar";
+import User from "../pages/User";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import PrivateRouter from "./PrivateRouter";
+import VerNav from "../components/VerNav";
 
 const AppRouter = () => {
-  const [user, setUser] = useState(false)
-  const [nav, setNav] = useState(false)
+  const [user, setUser] = useState(false);
+  const [nav, setNav] = useState(false);
 
   const handleClick = () => {
-    setNav(!nav)
-  }
+    setNav(!nav);
+  };
   return (
     <>
-    <Navbar user={user} nav={nav} handleClick={handleClick}/>
-        <Routes>
-            <Route path='/' element={<Home/>} />
-            <Route path='/login' element={<Login setUser={setUser}/>} />
-            <Route path='/register' element={<Register setUser={setUser}/>} />
-            <Route path='/user' element={<PrivateRouter user={user}/>}>
-              <Route path='' element={<User />} />
-            </Route>
-        </Routes>
+      <VerNav nav={nav} handleClick={handleClick}/>
+      <Navbar user={user} nav={nav} handleClick={handleClick} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login setUser={setUser} />} />
+        <Route path="/register" element={<Register setUser={setUser} />} />
+        <Route path="/user" element={<PrivateRouter user={user} />}>
+          <Route path="" element={<User />} />
+        </Route>
+      </Routes>
     </>
-  )
-}
+  );
+};
 
-export default AppRouter
+export default AppRouter;
