c9ee2a601c5e08f8236babe7e88ae10ef149fc6d
erol23 <hurkanrol15@gmail.com>
2023-04-25 19:02:19 +0200

    added form

--- src/pages/AddNewEntry.jsx
+++ src/pages/AddNewEntry.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
 
 const AddNewEntry = () => {
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
     <div className='flex h-screen justify-center items-center'>
-        <h1>
-            hello world
-        </h1>
+        <form className='flex flex-col gap-2' onSubmit={handleSubmit}>
+          <input type="date" className='border border-violet-500 bg-gray-50 w-[250px] rounded-md'/>
+          <select name="type" id="type" className='border border-violet-500 bg-gray-50 w-[250px] rounded-md'>
+            <option value="inflow">InFlow</option>
+            <option value="outflow">OutFlow</option>
+          </select>
+          <input list='account' placeholder='Choose Account' className='border border-violet-500 bg-gray-50 w-[250px] rounded-md'/>
+          <datalist id="account">
+            <option value="Garanti"/>
+            <option value="İşbankası"/>
+            <option value="Akbank"/>
+          </datalist>
+          <input type="number" placeholder='Enter amount' className='border border-violet-500 bg-gray-50 w-[250px] rounded-md'/>
+          <input type="text" placeholder='Enter desc...' className='border border-violet-500 bg-gray-50 w-[250px] rounded-md'/>
+          <input list="currency" placeholder='Choose Currency' className='border border-violet-500 bg-gray-50 w-[250px] rounded-md'/>
+          <datalist id='currency'>
+            <option value="Euro" />
+            <option value="Dolar" />
+            <option value="TL" />
+          </datalist>
+          <button type='submit' className='bg-violet-500 text-white rounded-md'>Add Entry</button>
+        </form>
     </div>
   )
 }
