e76e2339500b77f8a2463680d1d7e8fea2bd81a9
erol23 <hurkanrol15@gmail.com>
2023-04-25 19:59:21 +0200

    added table on home page

---
+++ src/helper/budgetData.js
@@ -0,0 +1,26 @@
+export const budgetData = [
+    {
+        date: "26.04.2023",
+        transactiontype: "Inflow",
+        account: "garanti",
+        amount: 2000,
+        description: "salary",
+        currency:"Euro"
+    },
+    {
+        date: "26.04.2023",
+        transactiontype: "Inflow",
+        account: "garanti",
+        amount: 2000,
+        description: "salary",
+        currency:"Euro"
+    },
+    {
+        date: "26.04.2023",
+        transactiontype: "Inflow",
+        account: "garanti",
+        amount: 2000,
+        description: "salary",
+        currency:"Euro"
+    }
+]
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,35 @@
-import React from "react";
+import { budgetData } from "../helper/budgetData";
 
 const Home = () => {
+  console.log(budgetData);
   return (
     <>
-      <div className="flex items-center h-screen">Home</div>
+      <div className="flex items-center h-screen w-full justify-center">
+        <table className="w-full">
+          <tr className="bg-gray-500">
+            <th>Date</th>
+            <th>Type</th>
+            <th>Account</th>
+            <th>Amount</th>
+            <th>Description</th>
+            <th>Currency</th>
+          </tr>
+          {
+            budgetData.map((budget) => {
+              return(
+                <tr>
+                  <th>{budget.date}</th>
+                  <th>{budget.transactiontype}</th>
+                  <th>{budget.account}</th>
+                  <th>{budget.amount}</th>
+                  <th>{budget.description}</th>
+                  <th>{budget.currency}</th>
+                </tr>
+              )
+            })
+          }
+        </table>
+      </div>
     </>
   );
 };
