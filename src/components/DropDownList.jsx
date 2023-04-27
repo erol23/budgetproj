f2e297dcfeae9ec7bddd2a4b8a379936c2d639b0
erol23 <hurkanrol15@gmail.com>
2023-04-27 20:16:22 +0200

    created dropdownlist

--- src/components/DropDownList.jsx
+++ src/components/DropDownList.jsx
@@ -1,18 +1,32 @@
 import { Dropdown } from "flowbite-react";
-import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const DropDownList = () => {
-    const navigate = useNavigate()
+    const [labelValue, setLabelValue] = useState("Choose Account")
+  const navigate = useNavigate();
   return (
-    <Dropdown label="" dismissOnClick={false} className="w-[150px] bg-white text-gray-50">
-      <Dropdown.Item>Dashboard</Dropdown.Item>
-      <Dropdown.Item>Settings</Dropdown.Item>
-      <Dropdown.Item>Earnings</Dropdown.Item>
-      <Dropdown.Item>Sign out</Dropdown.Item>
-      <button onClick={() => navigate("/newentry")}>Add new account</button>
-    </Dropdown>
-
+    <div className="border border-violet-500">
+      <Dropdown
+        label={labelValue}
+        inline={true}
+        className="w-[150px] bg-gray-50"
+      >
+        <ul onClick={(e) => setLabelValue(e.target.innerHTML) }>
+            <li>Hello World</li>
+            <li>enes</li>
+            <li>gizem</li>
+            <li>erkan</li>
+            <li>erol</li>
+            <li>Hello World</li>
+        </ul>
+        {/* <Dropdown.Item>Dashboard</Dropdown.Item>
+        <Dropdown.Item>Settings</Dropdown.Item>
+        <Dropdown.Item>Earnings</Dropdown.Item>
+        <Dropdown.Item>Sign out</Dropdown.Item> */}
+        <button onClick={() => navigate("/newentry")}>Add new account</button>
+      </Dropdown>
+    </div>
   );
 };
 
