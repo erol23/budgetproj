c438e5a0c34d2ce5b95bbca299f5c5a170950124
erol23 <hurkanrol15@gmail.com>
2023-05-19 23:42:32 +0200

    filtered array

--- src/components/DataList.jsx
+++ src/components/DataList.jsx
@@ -2,17 +2,37 @@ import { useState } from "react";
 
 const DataList = () => {
   const [vis, setVis] = useState(false);
+  const [enterVal, setEnterVal] = useState("")
 
+  const flow = ["Coffee",
+    "Laptop",
+    "Sunflower",
+    "Soccer ball",
+    "Guitar",
+    "Umbrella",
+    "Book",
+    "Sunglasses",
+    "Bicycle",
+    "Pizza"]
+
+  const result = flow.filter((el) => el.includes(enterVal) )
+
+  const handleSet = (e) => {
+    setEnterVal(e.target.innerHTML)
+    setVis(false)
+  }
+
+  console.log(result)
   return (
     <div>
-      <input type="text" onClick={() => setVis(true)} />
+      <input type="text" value={enterVal} onChange={(e) => setEnterVal(e.target.value)} onClick={() => setVis(true)} />
       {vis ? (
         <div className="absolute z-10">
-          <ul>
-            <li>Hello</li>
-            <li>Hello</li>
-            <li>Hello</li>
-            <li>Hello</li>
+          <ul onClick={handleSet}>
+            <li>Hello1</li>
+            <li>Hello2</li>
+            <li>Hello3</li>
+            <li>Hello4</li>
           </ul>
           <div>
             <button onClick={() => setVis(false)}>Add</button>
